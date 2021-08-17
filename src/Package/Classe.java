ecfaeb987cf2bca2477576a77fee4a3c0494539d
MichelPSilva <michelvpcontato@gmail.com>
2021-08-17 17:14:24 -0300

    Criando um novo repositório de exemplo

---
+++ src/Package/Classe.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package Package;
+
+/**
+ * @author Alunos
+ *
+ */
+public class Classe {
+
+	/**
+	 * 
+	 */
+	public Classe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("olá mundo!");
+
+	}
+
+}
